66782f656c7c4b525464a41b7db3218a63b0a20e
8b477 <jonathan_buchet@outlook.fr>
2023-08-15 16:25:01 +0200

    Add magic formula to get a percentage with scroll

--- scrollEvent2/Assets/Script/app.js
+++ scrollEvent2/Assets/Script/app.js
@@ -1,26 +1,32 @@
+let valueScroll = 0;
+
 window.addEventListener('scroll', () => {
 
+    // window.scrollY = la valeur du scroll actuel
+    // window.innerHeight = la taille de la barre de scroll
+    // document.body.offsetHeight = la hauteur de la page
+    // Permet d'avoir un pourcentage de scroll adaptative sur nimporte quelle taille d'écran
+    valueScroll = ((window.scrollY + window.innerHeight) / document.body.offsetHeight) * 100;
+
+    // Event on navbar
     if (window.scrollY > 50) {
         navbar.style.height = '40px';
     }
     else {
         navbar.style.height = '90px';
     }
-})
 
-
-window.addEventListener('scroll', () => {
-
-    if (window.scrollY > 110) {
+    // Event on first picture
+    if (valueScroll > 52) {
         imgImprovise.style.opacity = 1;
         imgImprovise.style.transform = 'translateX(0)';
     }
 
-    if (window.scrollY > 1200) {
+    //Event on popup
+    if (valueScroll > 85) {
         popup.style.opacity = 1;
         popup.style.transform = 'translateX(0)';
     }
-
 })
 
 closeBtn.addEventListener('click', () => {
